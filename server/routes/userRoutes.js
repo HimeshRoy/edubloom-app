1a004bad1f46c82ca1f91302c4921fcd0f88c9f3
HimeshRoy <himeshroy.cse@gmail.com>
2026-04-19 22:22:45 +0530

    1-1 chat problem fixed

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,14 +1,14 @@
 import express from "express";
 import { getProfile, updateProfile } from "../controllers/userController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
-
-
+import User from "../models/User.js";
 
 const router = express.Router();
 
 router.get("/me", authMiddleware, getProfile);
 router.put("/update", authMiddleware, updateProfile);
-router.get("/admin", auth, async (req, res) => {
+
+router.get("/admin", authMiddleware, async (req, res) => {
   const admin = await User.findOne({ role: "admin" }).select("-password");
   res.json(admin);
 });
