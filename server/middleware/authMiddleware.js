c4b04a4a89269a75235f1dba65b25dbb80d8fda0
HimeshRoy <himeshroy.cse@gmail.com>
2026-04-19 16:52:52 +0530

    admin dashboard added

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -13,6 +13,7 @@ export default function (req, res, next) {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     req.user = decoded;
+    console.log("DECODED 👉", decoded); // need to delete 
 
     next();
   } catch (err) {
